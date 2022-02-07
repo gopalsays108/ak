0c44bd077b25a1f8784c5fb824abcc6fe5b349a8
gopalsays108 <gopalsays108@gmail.com>
2022-02-08 01:58:26 +0530

    adding testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,6 +63,18 @@ const modalView = document.querySelectorAll('.services__modal'),
         })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true
+    },
+  });
 
 
 /*==================== TESTIMONIAL ====================*/
