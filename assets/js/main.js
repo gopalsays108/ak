bf2b4c3addac4bc9f3d46f947a95f2563c706700
gopalsays108 <gopalsays108@gmail.com>
2022-02-08 02:16:58 +0530

    completed testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,6 +78,21 @@ let swiper = new Swiper(".portfolio__container", {
 
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween:48,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+      dynamicBullets:true
+    },
+    breakpoints:{
+        568:{
+            slidesPerView:2,
+        }
+    },
+  });
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
